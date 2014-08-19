f29467fa28d0e8e61d448ed577b05b73287f91b4
ALlongueras <albert@albertllongueras.cat>
2014-08-19 18:54:32 +0200

    Added puntuation page

---
+++ Library/Helpers/PorraManagerUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Models;
+
+namespace Library.Helpers
+{
+    public class PorraManagerUtils
+    {
+        public static string ConvertObjectToJson(IEnumerable<PlayersInformation> informationList)
+        {
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(informationList);
+            return json;
+        }
+    }
+}
