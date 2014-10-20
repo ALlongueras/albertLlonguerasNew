8bbccd826f44083a67f18fe371fc92f40a144a60
ALlongueras <albert@albertllongueras.cat>
2014-10-21 00:04:42 +0200

    Added information page

---
+++ AlbertLlonguerasNew/scripts/informationPage.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    $(".click").click(function () {
+        var target = $(this).parent().children(".expand");
+        var targetAddIcon = $(this).parent().find(".addIcon");
+        var targetMinusIcon = $(this).parent().find(".minusIcon");
+        $(target).slideToggle();
+        if (targetAddIcon.hasClass("hidden")) {
+            $(targetAddIcon).removeClass("hidden");
+        } else {
+            $(targetAddIcon).addClass("hidden");
+        }
+        if (targetMinusIcon.hasClass("hidden")) {
+            $(targetMinusIcon).removeClass("hidden");
+        } else {
+            $(targetMinusIcon).addClass("hidden");
+        }
+
+    });
+});
