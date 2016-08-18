59a85bd06d3bce040b86c823aa28e5bee6148580
allongueras <xungue3914>
2016-08-17 23:53:44 -0400

    Send email to everyone

---
+++ AlbertLlonguerasNew/Controllers/EmailManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Reflection;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Core.Logging;
+
+namespace AlbertLlonguerasNew.Controllers
+{
+    public class EmailManager
+    {
+        public void SendEmail(string nom, string correu, string link, string linkName, string match)
+        {
+            var dataConf = this.GetDataSendMail();
+            if (!dataConf.Any()) return;
+            var port = Int32.Parse(dataConf["port"]);
+            var host = dataConf["host"];
+            var to = correu;
+            var pass = dataConf["passTo"];
+
+            var sb = new StringBuilder();
+            sb.AppendFormat("<p>{0} enrecorda't de fer la porra pel partit {1}</p>", nom, match);
+            sb.AppendFormat("<p>Porra disponible a: <a href=\"{0}\">{1}</a></p>", link, linkName);
+
+            var smtp = new SmtpClient(host, port) { EnableSsl = true };
+            var credentials = new NetworkCredential(to, pass);
+
+            var message = new MailMessage
+            {
+                From = new MailAddress(dataConf["mailTo"]),
+                To = { new MailAddress(to) },
+                Subject = "Nova porra " + match,
+                Body = sb.ToString(),
+                IsBodyHtml = true
+            };
+
+            try
+            {
+                smtp.EnableSsl = true;
+                smtp.Credentials = credentials;
+                smtp.Send(message);
+            }
+            catch (SmtpException smtpEx)
+            {
+            }
+        }
+
+        private Dictionary<string, string> GetDataSendMail()
+        {
+            var dataConf = new Dictionary<string, string>();
+
+            var mailTo = ConfigurationManager.AppSettings["sendMail:mail"];
+            var passTo = ConfigurationManager.AppSettings["sendMail:password"];
+            var host = ConfigurationManager.AppSettings["sendMail:host"];
+            var port = ConfigurationManager.AppSettings["sendMail:port"];
+
+            LogHelper.Info(MethodBase.GetCurrentMethod().DeclaringType, string.Format("SMTP configuration: {0}-{1}", host, port));
+
+            if (string.IsNullOrEmpty(mailTo) || string.IsNullOrEmpty(passTo) || string.IsNullOrEmpty(host) ||
+                string.IsNullOrEmpty(port)) return dataConf;
+            dataConf["mailTo"] = mailTo;
+            dataConf["passTo"] = passTo;
+            dataConf["host"] = host;
+            dataConf["port"] = port;
+
+            return dataConf;
+        }
+
+    }
+}
---
+++ AlbertLlonguerasNew/Controllers/ManageEvents.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+using Umbraco.Core.Publishing;
+using Umbraco.Core.Services;
+using Umbraco.Web;
+using Umbraco.Web.WebApi;
+
+namespace AlbertLlonguerasNew.Controllers
+{
+    public class ManageEvents:IApplicationEventHandler
+    {
+        public void OnApplicationInitialized(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+        }
+
+        public void OnApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            ContentService.Published += ContentService_CreatePrevia;
+        }
+
+        public void OnApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+        }
+
+        void ContentService_CreatePrevia(IPublishingStrategy sender, Umbraco.Core.Events.PublishEventArgs<Umbraco.Core.Models.IContent> e)
+        {
+            try
+            {
+                if (e.PublishedEntities.First().ContentType.Alias != "Previa")
+                {
+                    return;
+                }
+
+                if (e.PublishedEntities.First().GetValue<bool>("notified") == false)
+                {
+                    var createdNodeId = e.PublishedEntities.First().Id;
+                    var nodeContent = UmbracoContext.Current.Application.Services.ContentService.GetById(createdNodeId);
+                    var umbracoHelper = new UmbracoHelper(UmbracoContext.Current);
+                    var currentModel = umbracoHelper.TypedContent(createdNodeId);
+                    var matchLink = "http://albertllongueras.cat/porra";
+                    var matchLinkName = "Porra penya de l'espardenya";
+                    //var match = currentModel.GetPropertyValue<string>("title");
+                    var match = nodeContent.GetValue<string>("title");
+                    var membersList = ApplicationContext.Current.Services.MemberService.GetAllMembers();
+                    foreach (var member in membersList.Where(x => x.GetValue<bool>("notifyPrevia")))
+                    {
+                        var email = member.Email;
+                        var name = member.Name;
+                        var emailManager = new EmailManager();
+                        emailManager.SendEmail(name, email, matchLink, matchLinkName, match);
+                    }
+                    nodeContent.SetValue("notified", true);
+                    UmbracoContext.Current.Application.Services.ContentService.Save(nodeContent);
+                }
+
+            }
+            catch (Exception)
+            {
+                
+            }
+        }
+    }
+}
